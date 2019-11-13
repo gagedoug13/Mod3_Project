81e7ddea5e2b01b51b776d701b2738904e875a27
Gage Thornberry <gagedoug13@gmail.com>
2019-11-13 10:25:31 -0700

    pushing up presented project

---
+++ front_end/show.js
@@ -0,0 +1,76 @@
+const body = document.querySelector('body')
+
+function createShowCard(shows) {
+    const div = document.createElement('div')
+    div.className = "showCard"
+
+// const bandsForm = document.querySelector('.bands-form')
+//     button.addEventListener('submit', event => {
+//     event.preventDefault()
+    
+
+    shows.events.forEach(show => {
+        
+      
+
+        let divCard = document.createElement("div")
+        divCard.className = "eachShowCard"
+
+
+
+        const band = document.createElement('h3')
+        const date = document.createElement('p')
+        const venue = document.createElement('p')
+        const tickets = document.createElement('p')
+        const a_tag = document.createElement('a')
+        
+        a_tag.className = "band"
+        date.className = "date"
+        venue.className = "venue"
+        a_tag.href = `https://www.stubhub.com/${show.webURI}`
+        a_tag.target = "_blank"
+        margin1
+        
+        a_tag.innerHTML = `${show.performers[0].name}`
+        date.innerText = `${show.eventDateLocal.slice(0,10)}`
+        venue.innerText = `${show.venue.name}`
+        
+        if (show.ticketInfo.totalTickets == 0) {
+            tickets.innerText = "This show is sold out!"
+        } else if (show.ticketInfo.totalTickets == 1) {
+            tickets.innerText = "Only 1 ticket left!"
+        } else {
+        tickets.innerText = `Only ${show.ticketInfo.totalTickets} tickets left!`
+        }
+
+
+        divCard.append(a_tag, date, venue, tickets)
+        div.append(divCard)
+    })
+    body.append(div)
+}
+
+
+// fetch("http://localhost:3000/shows")
+//     .then(response => response.json())
+//     .then(createShowCard)
+
+
+
+    
+    const concertURL = "https://api.stubhub.com/sellers/search/events/v3?city=Denver&rows=100&categoryName=Concert"
+    
+    fetch(concertURL, {
+        method: "GET",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "Authorization": "Bearer b4ZVRidcGOowRYpSRN21cQuzxugC"
+        },
+      })
+      .then(response => response.json())
+      .then(shows => createShowCard(shows))
+      .catch(console.log)
+    //   .then(shows => console.log(shows.events))
+    
+
